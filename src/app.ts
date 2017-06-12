1fb19902fbf4e3412798bf8e8209900979052b3b
Benjamin Wallberg <me@bwallberg.com>
2017-06-12 20:48:02 +0200

    Added helper methods and added randomness to the tree
    Copied latest changed from RustFractalTree to this implementation

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@
     const width = 1280;
     const height = 720;
     const degrees_to_radians = Math.PI/180;
-    let depth = 9;
+    const angle_mod = 20;
 
     class Branch {
         start_x: number;
@@ -20,13 +20,29 @@
             this.split = false;
             this.cur_length = 0;
         }
+
+        get_end_x() {
+            return this.start_x + this.cur_length * Math.cos(this.angle * degrees_to_radians);
+        }
+
+        get_end_y() {
+            return this.start_y + this.cur_length * Math.sin(this.angle * degrees_to_radians);
+        }
+    }
+
+    function get_random_length(start_length) {
+        return start_length - (5 + (Math.random() * 15));
+    }
+
+    function get_random_angle(start_angle, direction) {
+        return start_angle + direction * (angle_mod + (Math.random() * 10));
     }
 
     function main() {
         const canvas = <HTMLCanvasElement> document.getElementById("canvas");
         const context = <CanvasRenderingContext2D> canvas.getContext("2d");
-        var angle_mod = 30,
-            branches: Branch[] = [];
+        var branches: Branch[] = [],
+            zero_reached = false;
 
         canvas.width = width;
         canvas.height = height;
@@ -39,36 +55,44 @@
         ));
 
         let update = function() {
-            if(depth > 0) {
+            if(zero_reached === false) {
                 let new_branches: Branch[] = [];
-                let did_split = false;
+                let nothing_updated = true;
 
                 branches.forEach((branch) => {
                     if(branch.split === false && branch.cur_length >= branch.length) {
+                        nothing_updated = false;
                         branch.split = true;
-                        did_split = true;
+                        
 
-                        new_branches.push(new Branch(
-                            branch.start_x + (branch.length * Math.cos(branch.angle * degrees_to_radians)),
-                            branch.start_y + (branch.length * Math.sin(branch.angle * degrees_to_radians)),
-                            branch.angle - angle_mod,
-                            branch.length - 7.5
-                        ));
+                        var branch_one = new Branch(
+                            branch.get_end_x(),
+                            branch.get_end_y(),
+                            get_random_angle(branch.angle, 1.0),
+                            get_random_length(branch.length)
+                        );
 
-                        new_branches.push(new Branch(
-                            branch.start_x + (branch.length * Math.cos(branch.angle * degrees_to_radians)),
-                            branch.start_y + (branch.length * Math.sin(branch.angle * degrees_to_radians)),
-                            branch.angle + angle_mod,
-                            branch.length - 7.5
-                        ));
+                        var branch_two = new Branch(
+                            branch.get_end_x(),
+                            branch.get_end_y(),
+                            get_random_angle(branch.angle, -1.0),
+                            get_random_length(branch.length)
+                        );
+
+                        if(branch_one.length > 0) {
+                            new_branches.push(branch_one);
+                        }
+                        if(branch_two.length > 0) {
+                            new_branches.push(branch_two);
+                        }
                     } else if(branch.cur_length < branch.length) {
+                        nothing_updated = false;
                         branch.cur_length += 4;
                     }
                 });
-
-                if(did_split) {
+                zero_reached = nothing_updated;
+                if(new_branches.length > 0) {
                     branches = branches.concat(new_branches);
-                    depth--;
                 }
             }
         };
